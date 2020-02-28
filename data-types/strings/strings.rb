3b4965a1191eda433cee77022c0724408933d417
Kayla Wood <kaylaewood@gmail.com>
2020-02-28 12:02:04 -0700

    Pass first 5 tests of data-typs/strings

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -2,58 +2,48 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 
-# There's a video walk-through of this exercise available here:
-# https://www.youtube.com/watch?v=BKqo2w0W7S0
-# and a rambling blog post on this general topic here:
-# https://josh.works/turing-backend-prep-02-first-tests-and-making-them-pass
-
 class StringTest < Minitest::Test
   def test_1
-    skip
     name = "alice"
     # In place of the line below, call a method on the name variable
     # defined above to acheive the expected output.
-    actual = name._____
+    actual = name.capitalize
     expected = "Alice"
 
     assert_equal expected, actual
   end
 
   def test_2
-    skip
     name = "aLiCe"
     # In place of the line below, call a method to achieve the expected output.
-    actual = name._____
+    actual = name.upcase
     expected = "ALICE"
 
     assert_equal expected, actual
   end
 
   def test_3
-    skip
     name = "AlIcE"
     # In place of the line below, call a method to achieve the expected output.
-    actual = name._____
+    actual = name.downcase
     expected = "alice"
 
     assert_equal expected, actual
   end
 
   def test_4
-    skip
     rhyme = "peter piper picked a peck of picked peppers"
     # In place of the line below, call a method to achieve the expected output.
-    actual = rhyme._____
+    actual = rhyme.reverse
     expected = "sreppep dekcip fo kcep a dekcip repip retep"
 
     assert_equal expected, actual
   end
 
   def test_5
-    skip
     word = "ticking"
     # In place of the line below, call a method to achieve the expected output.
-    actual = word.______
+    actual = word.gsub(/t/, "k")
     expected = "kicking"
 
     assert_equal expected, actual
